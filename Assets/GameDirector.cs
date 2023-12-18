17c9ef7cbed643ff5a687dea357ff61c77e1c0ed
A1PC21390\齋藤 弘樹 <hsaito@al-j.co.jp>
2023-12-18 13:33:39 +0900

    [add]初回コミット

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    // 「もう一度」ボタン
+    GameObject RetryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.RetryButton = GameObject.Find("RetryButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 「もう一度」を押下したらシーンをリロード
+    public void RetryButtonDown()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
